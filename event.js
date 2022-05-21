0849db7e561881dd82db590c255880b1326be923
Hins866 <104474796+Hins866@users.noreply.github.com>
2022-05-22 01:34:04 +0800

    Update event.js

--- event.js
+++ event.js
@@ -1,8 +1,8 @@
 AFRAME.registerComponent('markerhandler', {
 
     init: function() {
-        const animatedMarker = document.querySelector("#CP1-marker");
-        const text = document.querySelector("#CP1-bird");
+        const animatedMarker = document.querySelector("#CP1-bird");
+        const text = document.querySelector("#text-box");
 
         // every click, we make our model grow in size :)
         animatedMarker.addEventListener('click', function(event){
