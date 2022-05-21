441f29bf34ebd6f01febea9ea19f074da8eb86c4
Hins866 <104474796+Hins866@users.noreply.github.com>
2022-05-22 01:11:58 +0800

    update

--- event.js
+++ event.js
@@ -1,11 +1,13 @@
 AFRAME.registerComponent('markerhandler', {
 
     init: function() {
-        const animatedMarker = document.querySelector("CP1-marker");
-        const aEntity = document.querySelector("CP1-bird");
+        const animatedMarker = document.querySelector("#CP1-marker");
+        const text = document.querySelector("#CP1-bird");
 
         // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, target){
-            alert('click');
+        animatedMarker.addEventListener('click', function(event){
+            if(text.object3D.visible == false && event.detail.cursorEl){
+                text.object3D.visible = true;
+              } 
         });
     }});
