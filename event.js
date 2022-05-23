200d1f4ade9bc266b14abd6fba8f15b2115f7e02
Hins866 <104474796+Hins866@users.noreply.github.com>
2022-05-23 09:12:12 +0800

    update

--- event.js
+++ event.js
@@ -1,14 +1,7 @@
-AFRAME.registerComponent('markerhandler', {
-
-    init: function() {
-        const animatedMarker = document.querySelector("#CP1-bird");
-        const text = document.querySelector("#text-box");
-
-        // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(event){
-            alert("Hi i am a bird");
-            if(text.object3D.visible == false && event.detail.cursorEl){
-                text.object3D.visible = true;
-              } 
-        });
-    }});
+AFRAME.registerComponent('cursor-listener', {
+    init: function () {
+    this.el.addEventListener('click', function (evt) {
+      alert("testing");
+      });
+    }
+    });
