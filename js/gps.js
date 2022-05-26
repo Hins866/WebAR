05c42318a4a0eb9818ae7fc1056e9f4f2c70a92e
Hins866 <104474796+Hins866@users.noreply.github.com>
2022-05-26 13:52:13 +0800

    uodate

--- js/gps.js
+++ js/gps.js
@@ -1,6 +1,9 @@
 var latitude = prompt("latitude"); 
 var longitude = prompt("longitude");
 const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
+function ShowInput() {
+    alert('distanceMsg','latitude','longitude');
+  }
 alert('distanceMsg','latitude','longitude');   // "890 meters"
 console.log(latitude,longitude);
 AFRAME.registerComponent('school-playground', {
