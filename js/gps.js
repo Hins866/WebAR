93100137a52b14233281caa73845c00e7ed6614b
Hins866 <104474796+Hins866@users.noreply.github.com>
2022-05-26 09:29:23 +0800

    update

--- js/gps.js
+++ js/gps.js
@@ -1,4 +1,17 @@
 var latitude = prompt("latitude"); 
 var longitude = prompt("longitude");
-
+const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
+alert(distanceMsg,latitude,longitude);   // "890 meters"
 console.log(latitude,longitude);
+AFRAME.registerComponent('school-playground', {
+    /**
+     * Code within this function will be called when everything in <a-scene> is ready and loaded.
+     */
+    init: function () {
+      // Add code here!
+      console.log('Welcome, class!');
+      var sceneEl = document.querySelector('a-scene'); 
+      sceneEl.querySelector('a-entity').setAttribute('gps-entity-place', {x: 0, y: 0, z: 0});
+      
+    }
+  });
