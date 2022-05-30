a998ea5b40f46559c12a2aa5b9e38ec570627db0
Hins866 <104474796+Hins866@users.noreply.github.com>
2022-05-31 04:35:30 +0800

    update

--- js/gps.js
+++ js/gps.js
@@ -1,23 +1,25 @@
-//Ask user Input the location
-var lat = prompt('latitude'); 
-var long = prompt('longitude');
-var input = ('latitude: '+ lat +'; longitude: '+ long)
-
 //Modify the model location by user input
 AFRAME.registerComponent(' Modifying Entities', {
-        init: function () {
-          var sceneEl = document.querySelector('a-scene'); 
-          var gpsEl = sceneEl.querySelector('a-entity')
-  
-          gpsEl.addEventListener('gps-update', function () {
-            gpsEl.setAttribute('gps-entity-place', input);  
-          });
-          gpsEl.emit('gps-update');
-        }
-      });; 
+  init: function () {
+    var sceneEl = document.querySelector('a-scene'); 
+    var gpsEl = sceneEl.querySelector('a-entity')
+
+    gpsEl.addEventListener('gps-update', function () {
+      gpsEl.setAttribute('gps-entity-place', input);  
+    });
+    gpsEl.emit('gps-update');
+  }
+});; 
+
+//Ask user Input the location
+function Input(){
+var lat = prompt('latitude'); 
+var long = prompt('longitude');
+this.input = ('latitude: '+ lat +'; longitude: '+ long)
+sessionStorage.setItem('checkedpoint','CP4');
+}
 
 //Button for user check inputed value
 function ShowInput(){
-    alert(input);
-    }
-  
+alert(input);
+}
