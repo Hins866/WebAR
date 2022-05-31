8ee465c016ef992d0658d4a6b0f38bb16fa10c2e
Hins866 <104474796+Hins866@users.noreply.github.com>
2022-05-31 17:38:34 +0800

    update

--- js/gps.js
+++ js/gps.js
@@ -1,15 +1,20 @@
 //Modify the model location by user input
-AFRAME.registerComponent('Modifying Entities', {
+AFRAME.registerComponent('modifying-entities', {
   init: function () {
-    var sceneEl = document.querySelector('a-scene'); 
+    var sceneEl = document.querySelector('a-scene');
     var gpsEl = sceneEl.querySelector('a-entity')
-
+    console.log(sceneEl.querySelector('a-entity'));
+    console.log(gpsEl);
+    //Why i can't get the gps-entity-place??? 
+    console.log(gpsEl.querySelector('gps-entity-place'));
+/**    
     gpsEl.addEventListener('gps-update', function () {
       gpsEl.setAttribute('gps-entity-place', input);  
     });
     gpsEl.emit('gps-update');
-  }
-});; 
+*/  
+  } 
+})
 
 //Ask user Input the location
 function Input(){
